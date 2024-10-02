b0ca3a89c8ee387160c676ce71f16b79d662f15b
Eduardo de Lima Balbinot <914thepro@gmail.com>
2024-10-02 19:02:13 +0000

    Publish 1 from staging into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -6,64 +6,66 @@ import * as $$$$$$$$$0 from "./actions/sendTcoUserData.ts";
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Accordion.tsx";
-import * as $$$$$$1 from "./sections/BigImageHero.tsx";
-import * as $$$$$$2 from "./sections/CalculateYourCosts.tsx";
-import * as $$$$$$3 from "./sections/Carousel.tsx";
-import * as $$$$$$4 from "./sections/CollapseGroup.tsx";
-import * as $$$$$$5 from "./sections/DetailedCarousel.tsx";
-import * as $$$$$$6 from "./sections/Footer.tsx";
-import * as $$$$$$7 from "./sections/Footer2.tsx";
-import * as $$$$$$8 from "./sections/Header.tsx";
-import * as $$$$$$9 from "./sections/Header2.tsx";
-import * as $$$$$$10 from "./sections/Hero.tsx";
-import * as $$$$$$11 from "./sections/HeroCard.tsx";
-import * as $$$$$$12 from "./sections/HeroInfo.tsx";
-import * as $$$$$$13 from "./sections/HeroTab.tsx";
-import * as $$$$$$14 from "./sections/HeroWithTime.tsx";
-import * as $$$$$$15 from "./sections/ItWorks.tsx";
-import * as $$$$$$16 from "./sections/Logos.tsx";
-import * as $$$$$$17 from "./sections/MainHero.tsx";
-import * as $$$$$$18 from "./sections/MigrateToUs.tsx";
-import * as $$$$$$19 from "./sections/Partners.tsx";
-import * as $$$$$$20 from "./sections/PlanDetails.tsx";
-import * as $$$$$$21 from "./sections/PlanDetails2.tsx";
-import * as $$$$$$22 from "./sections/Plans.tsx";
-import * as $$$$$$23 from "./sections/Prices.tsx";
-import * as $$$$$$24 from "./sections/TalkToAnExpert.tsx";
-import * as $$$$$$25 from "./sections/TcoCalculator.tsx";
-import * as $$$$$$26 from "./sections/Testimonials.tsx";
-import * as $$$$$$27 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/BackToTopButton.tsx";
+import * as $$$$$$2 from "./sections/BigImageHero.tsx";
+import * as $$$$$$3 from "./sections/CalculateYourCosts.tsx";
+import * as $$$$$$4 from "./sections/Carousel.tsx";
+import * as $$$$$$5 from "./sections/CollapseGroup.tsx";
+import * as $$$$$$6 from "./sections/DetailedCarousel.tsx";
+import * as $$$$$$7 from "./sections/Footer.tsx";
+import * as $$$$$$8 from "./sections/Footer2.tsx";
+import * as $$$$$$9 from "./sections/Header.tsx";
+import * as $$$$$$10 from "./sections/Header2.tsx";
+import * as $$$$$$11 from "./sections/Hero.tsx";
+import * as $$$$$$12 from "./sections/HeroCard.tsx";
+import * as $$$$$$13 from "./sections/HeroInfo.tsx";
+import * as $$$$$$14 from "./sections/HeroTab.tsx";
+import * as $$$$$$15 from "./sections/HeroWithTime.tsx";
+import * as $$$$$$16 from "./sections/ItWorks.tsx";
+import * as $$$$$$17 from "./sections/Logos.tsx";
+import * as $$$$$$18 from "./sections/MainHero.tsx";
+import * as $$$$$$19 from "./sections/MigrateToUs.tsx";
+import * as $$$$$$20 from "./sections/Partners.tsx";
+import * as $$$$$$21 from "./sections/PlanDetails.tsx";
+import * as $$$$$$22 from "./sections/PlanDetails2.tsx";
+import * as $$$$$$23 from "./sections/Plans.tsx";
+import * as $$$$$$24 from "./sections/Prices.tsx";
+import * as $$$$$$25 from "./sections/TalkToAnExpert.tsx";
+import * as $$$$$$26 from "./sections/TcoCalculator.tsx";
+import * as $$$$$$27 from "./sections/Testimonials.tsx";
+import * as $$$$$$28 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Accordion.tsx": $$$$$$0,
-    "site/sections/BigImageHero.tsx": $$$$$$1,
-    "site/sections/CalculateYourCosts.tsx": $$$$$$2,
-    "site/sections/Carousel.tsx": $$$$$$3,
-    "site/sections/CollapseGroup.tsx": $$$$$$4,
-    "site/sections/DetailedCarousel.tsx": $$$$$$5,
-    "site/sections/Footer.tsx": $$$$$$6,
-    "site/sections/Footer2.tsx": $$$$$$7,
-    "site/sections/Header.tsx": $$$$$$8,
-    "site/sections/Header2.tsx": $$$$$$9,
-    "site/sections/Hero.tsx": $$$$$$10,
-    "site/sections/HeroCard.tsx": $$$$$$11,
-    "site/sections/HeroInfo.tsx": $$$$$$12,
-    "site/sections/HeroTab.tsx": $$$$$$13,
-    "site/sections/HeroWithTime.tsx": $$$$$$14,
-    "site/sections/ItWorks.tsx": $$$$$$15,
-    "site/sections/Logos.tsx": $$$$$$16,
-    "site/sections/MainHero.tsx": $$$$$$17,
-    "site/sections/MigrateToUs.tsx": $$$$$$18,
-    "site/sections/Partners.tsx": $$$$$$19,
-    "site/sections/PlanDetails.tsx": $$$$$$20,
-    "site/sections/PlanDetails2.tsx": $$$$$$21,
-    "site/sections/Plans.tsx": $$$$$$22,
-    "site/sections/Prices.tsx": $$$$$$23,
-    "site/sections/TalkToAnExpert.tsx": $$$$$$24,
-    "site/sections/TcoCalculator.tsx": $$$$$$25,
-    "site/sections/Testimonials.tsx": $$$$$$26,
-    "site/sections/Theme/Theme.tsx": $$$$$$27,
+    "site/sections/BackToTopButton.tsx": $$$$$$1,
+    "site/sections/BigImageHero.tsx": $$$$$$2,
+    "site/sections/CalculateYourCosts.tsx": $$$$$$3,
+    "site/sections/Carousel.tsx": $$$$$$4,
+    "site/sections/CollapseGroup.tsx": $$$$$$5,
+    "site/sections/DetailedCarousel.tsx": $$$$$$6,
+    "site/sections/Footer.tsx": $$$$$$7,
+    "site/sections/Footer2.tsx": $$$$$$8,
+    "site/sections/Header.tsx": $$$$$$9,
+    "site/sections/Header2.tsx": $$$$$$10,
+    "site/sections/Hero.tsx": $$$$$$11,
+    "site/sections/HeroCard.tsx": $$$$$$12,
+    "site/sections/HeroInfo.tsx": $$$$$$13,
+    "site/sections/HeroTab.tsx": $$$$$$14,
+    "site/sections/HeroWithTime.tsx": $$$$$$15,
+    "site/sections/ItWorks.tsx": $$$$$$16,
+    "site/sections/Logos.tsx": $$$$$$17,
+    "site/sections/MainHero.tsx": $$$$$$18,
+    "site/sections/MigrateToUs.tsx": $$$$$$19,
+    "site/sections/Partners.tsx": $$$$$$20,
+    "site/sections/PlanDetails.tsx": $$$$$$21,
+    "site/sections/PlanDetails2.tsx": $$$$$$22,
+    "site/sections/Plans.tsx": $$$$$$23,
+    "site/sections/Prices.tsx": $$$$$$24,
+    "site/sections/TalkToAnExpert.tsx": $$$$$$25,
+    "site/sections/TcoCalculator.tsx": $$$$$$26,
+    "site/sections/Testimonials.tsx": $$$$$$27,
+    "site/sections/Theme/Theme.tsx": $$$$$$28,
   },
   "actions": {
     "site/actions/sendTcoUserData.ts": $$$$$$$$$0,

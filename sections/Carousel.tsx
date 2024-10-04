de5589012a11945e874fcf942083bc562b9424d4
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-10-04 16:30:40 -0300

    feat: now Carousel can have TalkToSpecialist CTAs

--- sections/Carousel.tsx
+++ sections/Carousel.tsx
@@ -4,6 +4,7 @@ import Slider from "../components/ui/Slider2.tsx";
 import { useId } from "../sdk/useId.ts";
 import { useScript } from "@deco/deco/hooks";
 import AnimateOnShow from "../components/ui/AnimateOnShow.tsx"
+import TalkToSpecialistCta from "site/components/TalkToSpecialitCta.tsx";
 
 export interface CarouselIcon {
     src: ImageWidget;
@@ -180,19 +181,28 @@ function Carousel(props: Props) {
                 {/* {props.dots && <Dots slides={slides} interval={interval} />}{" "} */}
                 {props.arrows && <Buttons arrowsColor={arrowsColor} />}
             </AnimateOnShow >
-            {cta && <div id="carouselButtons" class="flex justify-center gap-7 mt-4">
-                {cta.map((button) => (<a
-                    href={button?.href ?? "#"}
-                    target={button?.href.includes("http") ? "_blank" : "_self"}
-                    class={`${button.ctaStyle != "link" && 'btn btn-primary px-7'} flex items-center self-start gap-1 border-primary font-bold hover:scale-110 transition-transform text-lg`}
-                    style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
-                >
-                    {button?.text}
-                    {button.underlineText && <span class="underline">{button.underlineText}</span>}
-                    {button.showIcon && <svg width="20" height="20" viewBox="0 0 20 20" class="fill-current" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15.3941 4.50977V12.2285C15.3941 12.386 15.3316 12.537 15.2202 12.6484C15.1089 12.7597 14.9579 12.8223 14.8004 12.8223C14.6429 12.8223 14.4919 12.7597 14.3805 12.6484C14.2692 12.537 14.2066 12.386 14.2066 12.2285V5.94293L5.72046 14.4298C5.60905 14.5413 5.45794 14.6038 5.30038 14.6038C5.14282 14.6038 4.99171 14.5413 4.8803 14.4298C4.76889 14.3184 4.7063 14.1673 4.7063 14.0098C4.7063 13.8522 4.76889 13.7011 4.8803 13.5897L13.3672 5.10352H7.08163C6.92416 5.10352 6.77313 5.04096 6.66178 4.92961C6.55043 4.81826 6.48788 4.66724 6.48788 4.50977C6.48788 4.35229 6.55043 4.20127 6.66178 4.08992C6.77313 3.97857 6.92416 3.91602 7.08163 3.91602H14.8004C14.9579 3.91602 15.1089 3.97857 15.2202 4.08992C15.3316 4.20127 15.3941 4.35229 15.3941 4.50977Z" />
-                    </svg>}
-                </a>))}
+            {cta && <div id="carouselButtons" class="flex flex-wrap justify-center gap-7 mt-4 px-7">
+                {cta.map((button) => {
+                    if (button.href == '/talkToSpecialist') return <TalkToSpecialistCta
+                        showIcon={button.showIcon}
+                        underlineText={button.underlineText}
+                        text={button.text}
+                        ctaClass={`${button.ctaStyle != "link" && 'btn btn-primary px-7'} h-auto flex items-center gap-1 border-primary font-bold hover:scale-110 transition-transform text-lg cursor-pointer`}
+                        style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
+                    />
+                    return <a
+                        href={button?.href ?? "#"}
+                        target={button?.href.includes("http") ? "_blank" : ""}
+                        class={`${button.ctaStyle != "link" && 'btn btn-primary px-7'} h-auto flex items-center gap-1 border-primary font-bold hover:scale-110 transition-transform text-lg`}
+                        style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
+                    >
+                        {button?.text}
+                        {button.underlineText && <span class="underline">{button.underlineText}</span>}
+                        {button.showIcon && <svg width="20" height="20" viewBox="0 0 20 20" class="fill-current" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.3941 4.50977V12.2285C15.3941 12.386 15.3316 12.537 15.2202 12.6484C15.1089 12.7597 14.9579 12.8223 14.8004 12.8223C14.6429 12.8223 14.4919 12.7597 14.3805 12.6484C14.2692 12.537 14.2066 12.386 14.2066 12.2285V5.94293L5.72046 14.4298C5.60905 14.5413 5.45794 14.6038 5.30038 14.6038C5.14282 14.6038 4.99171 14.5413 4.8803 14.4298C4.76889 14.3184 4.7063 14.1673 4.7063 14.0098C4.7063 13.8522 4.76889 13.7011 4.8803 13.5897L13.3672 5.10352H7.08163C6.92416 5.10352 6.77313 5.04096 6.66178 4.92961C6.55043 4.81826 6.48788 4.66724 6.48788 4.50977C6.48788 4.35229 6.55043 4.20127 6.66178 4.08992C6.77313 3.97857 6.92416 3.91602 7.08163 3.91602H14.8004C14.9579 3.91602 15.1089 3.97857 15.2202 4.08992C15.3316 4.20127 15.3941 4.35229 15.3941 4.50977Z" />
+                        </svg>}
+                    </a>
+                })}
             </div>}
         </div>
     </div>);

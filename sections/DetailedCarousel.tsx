72e85150b3e1dbe83c64af3bdce11362b6cebeba
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-10-10 01:23:15 -0300

    style: DetailedCarousel dots text align changed to left

--- sections/DetailedCarousel.tsx
+++ sections/DetailedCarousel.tsx
@@ -20,7 +20,6 @@ const refreshArrowsVisibility = () => {
                 
                 const startDistance = carousel.getBoundingClientRect().left - carouselItems[0].getBoundingClientRect().left;
                 const endDistance = carouselItems[carouselItems.length - 1].getBoundingClientRect().right - carousel.getBoundingClientRect().right;
-                console.log(endDistance);
                 const prevButton = currentTarget.querySelector(".prev-button") as HTMLElement | null | undefined;
                 const nextButton = currentTarget.querySelector(".next-button") as HTMLElement | null | undefined;
     
@@ -156,7 +155,7 @@ function Dots({ slides, interval = 0, dotsColor }: Props) {
                         <Slider.Dot index={index}>
                             <div class="py-5 h-full">
                                 <div class="h-0 w-11 opacity-0 md:opacity-100 md:h-auto md:w-auto">
-                                    <p class="text-lg text-primary font-semibold opacity-30 group-disabled:opacity-100" style={{ color: dotsColor }}>{slide.title}</p>
+                                    <p class="text-lg text-primary text-left font-semibold opacity-30 group-disabled:opacity-100" style={{ color: dotsColor }}>{slide.title}</p>
                                 </div>
                                 <div class="h-1 mt-2 rounded-full dot overflow-hidden bg-accent-content" style={{ backgroundColor: '#A1ABBC' }}>
                                     <div class="h-full w-0 bg-primary group-disabled:animate-progress" style={{ animationDuration: `${interval}s`, backgroundColor: dotsColor }} />
--- sections/Plans.tsx
+++ sections/Plans.tsx
@@ -9,7 +9,6 @@ import CreateStoreCta from "site/components/CreateStoreCta.tsx";
 
 const onClick = (rootId: string) => {
     const parent = document.getElementById(rootId) as HTMLElement;
-    console.log(parent);
     const fullBulletPoints = parent?.querySelector("#" + rootId + "fullBulletPoints") as HTMLElement;
     const firstBulletPoints = parent?.querySelector("#" + rootId + "firstBulletPoints");
     firstBulletPoints?.classList.add("hidden");
@@ -65,7 +64,6 @@ const refreshArrowsVisibility = () => {
                 
                 const startDistance = carousel.getBoundingClientRect().left - carouselItems[0].getBoundingClientRect().left;
                 const endDistance = carouselItems[carouselItems.length - 1].getBoundingClientRect().right - carousel.getBoundingClientRect().right;
-                console.log(endDistance);
                 const prevButton = currentTarget.querySelector(".prev-button") as HTMLElement | null | undefined;
                 const nextButton = currentTarget.querySelector(".next-button") as HTMLElement | null | undefined;
     
@@ -337,7 +335,7 @@ function Buttons({arrowsColor}: {arrowsColor?: string}) {
             </div>
             <div class="flex items-center justify-center z-10 ">
                 <Slider.NextButton class="flex items-center justify-center ">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" class="text-primary fill-current next-button opacity-20 transition-opacity" style={{color: arrowsColor}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" class="text-primary fill-current next-button transition-opacity" style={{color: arrowsColor}}>
                         <path d="M25.3835 4.67773C21.4279 4.67773 17.5611 5.85071 14.2721 8.04834C10.9831 10.246 8.41969 13.3695 6.90593 17.0241C5.39218 20.6786 4.99611 24.6999 5.76782 28.5795C6.53952 32.4592 8.44434 36.0228 11.2414 38.8199C14.0384 41.6169 17.6021 43.5217 21.4817 44.2934C25.3613 45.0651 29.3827 44.6691 33.0372 43.1553C36.6917 41.6416 39.8153 39.0781 42.0129 35.7891C44.2105 32.5002 45.3835 28.6334 45.3835 24.6777C45.3779 19.3751 43.269 14.2913 39.5195 10.5418C35.77 6.79227 30.6861 4.68333 25.3835 4.67773ZM31.0874 25.7662L23.3951 33.4585C23.2521 33.6014 23.0824 33.7148 22.8957 33.7922C22.7089 33.8695 22.5087 33.9094 22.3066 33.9094C22.1045 33.9094 21.9043 33.8695 21.7175 33.7922C21.5308 33.7148 21.3611 33.6014 21.2181 33.4585C21.0752 33.3156 20.9618 33.1459 20.8845 32.9591C20.8071 32.7724 20.7673 32.5722 20.7673 32.37C20.7673 32.1679 20.8071 31.9677 20.8845 31.781C20.9618 31.5942 21.0752 31.4245 21.2181 31.2816L27.8239 24.6777L21.2181 18.0739C20.9295 17.7852 20.7673 17.3937 20.7673 16.9854C20.7673 16.5772 20.9295 16.1856 21.2181 15.897C21.5068 15.6083 21.8983 15.4461 22.3066 15.4461C22.7149 15.4461 23.1064 15.6083 23.3951 15.897L31.0874 23.5893C31.2304 23.7322 31.3439 23.9018 31.4213 24.0886C31.4987 24.2754 31.5386 24.4756 31.5386 24.6777C31.5386 24.8799 31.4987 25.0801 31.4213 25.2669C31.3439 25.4536 31.2304 25.6233 31.0874 25.7662Z"/>
                     </svg>
                 </Slider.NextButton>

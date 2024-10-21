460e80437ada6763379294c0ac6b97a52eefc291
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-10-21 00:54:00 -0300

    feat: Faq section done

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -14,34 +14,35 @@ import * as $$$$$$5 from "./sections/Carousel.tsx";
 import * as $$$$$$6 from "./sections/CollapseGroup.tsx";
 import * as $$$$$$7 from "./sections/DetailedCarousel.tsx";
 import * as $$$$$$8 from "./sections/DetailedHero.tsx";
-import * as $$$$$$9 from "./sections/FloatingButtons.tsx";
-import * as $$$$$$10 from "./sections/Footer.tsx";
-import * as $$$$$$11 from "./sections/Footer2.tsx";
-import * as $$$$$$12 from "./sections/Header.tsx";
-import * as $$$$$$13 from "./sections/Header2.tsx";
-import * as $$$$$$14 from "./sections/Hero.tsx";
-import * as $$$$$$15 from "./sections/HeroCard.tsx";
-import * as $$$$$$16 from "./sections/HeroInfo.tsx";
-import * as $$$$$$17 from "./sections/HeroTab.tsx";
-import * as $$$$$$18 from "./sections/HeroWithTime.tsx";
-import * as $$$$$$19 from "./sections/ItWorks.tsx";
-import * as $$$$$$20 from "./sections/Logos.tsx";
-import * as $$$$$$21 from "./sections/MainHero.tsx";
-import * as $$$$$$22 from "./sections/MigrateToUs.tsx";
-import * as $$$$$$23 from "./sections/Partners.tsx";
-import * as $$$$$$24 from "./sections/PlanDetails.tsx";
-import * as $$$$$$25 from "./sections/PlanDetails2.tsx";
-import * as $$$$$$26 from "./sections/Plans.tsx";
-import * as $$$$$$27 from "./sections/Prices.tsx";
-import * as $$$$$$28 from "./sections/Quote.tsx";
-import * as $$$$$$29 from "./sections/ScrollOnLoadScript.tsx";
-import * as $$$$$$30 from "./sections/SimpleHero.tsx";
-import * as $$$$$$31 from "./sections/TalkToAnExpert.tsx";
-import * as $$$$$$32 from "./sections/TalkToSpecialistModal.tsx";
-import * as $$$$$$33 from "./sections/TcoCalculator.tsx";
-import * as $$$$$$34 from "./sections/TcoCalculatorV2.tsx";
-import * as $$$$$$35 from "./sections/Testimonials.tsx";
-import * as $$$$$$36 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$9 from "./sections/Faq.tsx";
+import * as $$$$$$10 from "./sections/FloatingButtons.tsx";
+import * as $$$$$$11 from "./sections/Footer.tsx";
+import * as $$$$$$12 from "./sections/Footer2.tsx";
+import * as $$$$$$13 from "./sections/Header.tsx";
+import * as $$$$$$14 from "./sections/Header2.tsx";
+import * as $$$$$$15 from "./sections/Hero.tsx";
+import * as $$$$$$16 from "./sections/HeroCard.tsx";
+import * as $$$$$$17 from "./sections/HeroInfo.tsx";
+import * as $$$$$$18 from "./sections/HeroTab.tsx";
+import * as $$$$$$19 from "./sections/HeroWithTime.tsx";
+import * as $$$$$$20 from "./sections/ItWorks.tsx";
+import * as $$$$$$21 from "./sections/Logos.tsx";
+import * as $$$$$$22 from "./sections/MainHero.tsx";
+import * as $$$$$$23 from "./sections/MigrateToUs.tsx";
+import * as $$$$$$24 from "./sections/Partners.tsx";
+import * as $$$$$$25 from "./sections/PlanDetails.tsx";
+import * as $$$$$$26 from "./sections/PlanDetails2.tsx";
+import * as $$$$$$27 from "./sections/Plans.tsx";
+import * as $$$$$$28 from "./sections/Prices.tsx";
+import * as $$$$$$29 from "./sections/Quote.tsx";
+import * as $$$$$$30 from "./sections/ScrollOnLoadScript.tsx";
+import * as $$$$$$31 from "./sections/SimpleHero.tsx";
+import * as $$$$$$32 from "./sections/TalkToAnExpert.tsx";
+import * as $$$$$$33 from "./sections/TalkToSpecialistModal.tsx";
+import * as $$$$$$34 from "./sections/TcoCalculator.tsx";
+import * as $$$$$$35 from "./sections/TcoCalculatorV2.tsx";
+import * as $$$$$$36 from "./sections/Testimonials.tsx";
+import * as $$$$$$37 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -54,34 +55,35 @@ const manifest = {
     "site/sections/CollapseGroup.tsx": $$$$$$6,
     "site/sections/DetailedCarousel.tsx": $$$$$$7,
     "site/sections/DetailedHero.tsx": $$$$$$8,
-    "site/sections/FloatingButtons.tsx": $$$$$$9,
-    "site/sections/Footer.tsx": $$$$$$10,
-    "site/sections/Footer2.tsx": $$$$$$11,
-    "site/sections/Header.tsx": $$$$$$12,
-    "site/sections/Header2.tsx": $$$$$$13,
-    "site/sections/Hero.tsx": $$$$$$14,
-    "site/sections/HeroCard.tsx": $$$$$$15,
-    "site/sections/HeroInfo.tsx": $$$$$$16,
-    "site/sections/HeroTab.tsx": $$$$$$17,
-    "site/sections/HeroWithTime.tsx": $$$$$$18,
-    "site/sections/ItWorks.tsx": $$$$$$19,
-    "site/sections/Logos.tsx": $$$$$$20,
-    "site/sections/MainHero.tsx": $$$$$$21,
-    "site/sections/MigrateToUs.tsx": $$$$$$22,
-    "site/sections/Partners.tsx": $$$$$$23,
-    "site/sections/PlanDetails.tsx": $$$$$$24,
-    "site/sections/PlanDetails2.tsx": $$$$$$25,
-    "site/sections/Plans.tsx": $$$$$$26,
-    "site/sections/Prices.tsx": $$$$$$27,
-    "site/sections/Quote.tsx": $$$$$$28,
-    "site/sections/ScrollOnLoadScript.tsx": $$$$$$29,
-    "site/sections/SimpleHero.tsx": $$$$$$30,
-    "site/sections/TalkToAnExpert.tsx": $$$$$$31,
-    "site/sections/TalkToSpecialistModal.tsx": $$$$$$32,
-    "site/sections/TcoCalculator.tsx": $$$$$$33,
-    "site/sections/TcoCalculatorV2.tsx": $$$$$$34,
-    "site/sections/Testimonials.tsx": $$$$$$35,
-    "site/sections/Theme/Theme.tsx": $$$$$$36,
+    "site/sections/Faq.tsx": $$$$$$9,
+    "site/sections/FloatingButtons.tsx": $$$$$$10,
+    "site/sections/Footer.tsx": $$$$$$11,
+    "site/sections/Footer2.tsx": $$$$$$12,
+    "site/sections/Header.tsx": $$$$$$13,
+    "site/sections/Header2.tsx": $$$$$$14,
+    "site/sections/Hero.tsx": $$$$$$15,
+    "site/sections/HeroCard.tsx": $$$$$$16,
+    "site/sections/HeroInfo.tsx": $$$$$$17,
+    "site/sections/HeroTab.tsx": $$$$$$18,
+    "site/sections/HeroWithTime.tsx": $$$$$$19,
+    "site/sections/ItWorks.tsx": $$$$$$20,
+    "site/sections/Logos.tsx": $$$$$$21,
+    "site/sections/MainHero.tsx": $$$$$$22,
+    "site/sections/MigrateToUs.tsx": $$$$$$23,
+    "site/sections/Partners.tsx": $$$$$$24,
+    "site/sections/PlanDetails.tsx": $$$$$$25,
+    "site/sections/PlanDetails2.tsx": $$$$$$26,
+    "site/sections/Plans.tsx": $$$$$$27,
+    "site/sections/Prices.tsx": $$$$$$28,
+    "site/sections/Quote.tsx": $$$$$$29,
+    "site/sections/ScrollOnLoadScript.tsx": $$$$$$30,
+    "site/sections/SimpleHero.tsx": $$$$$$31,
+    "site/sections/TalkToAnExpert.tsx": $$$$$$32,
+    "site/sections/TalkToSpecialistModal.tsx": $$$$$$33,
+    "site/sections/TcoCalculator.tsx": $$$$$$34,
+    "site/sections/TcoCalculatorV2.tsx": $$$$$$35,
+    "site/sections/Testimonials.tsx": $$$$$$36,
+    "site/sections/Theme/Theme.tsx": $$$$$$37,
   },
   "actions": {
     "site/actions/sendTcoUserData.ts": $$$$$$$$$0,
---
+++ sections/Faq.tsx
@@ -0,0 +1,107 @@
+import type { HTMLWidget } from "apps/admin/widgets.ts";
+import { useScript } from "@deco/deco/hooks";
+import AnimateOnShow from "../components/ui/AnimateOnShow.tsx";
+import TalkToSpecialistCta from "site/components/TalkToSpecialitCta.tsx";
+
+const onChange = () => {
+    const element = event!.currentTarget as HTMLInputElement;
+    const arrow = element.parentElement?.querySelector(".collapse-arrow") as HTMLElement;
+    const secondaryHeaderItems = element.parentElement?.querySelectorAll(".secondaryHeaderItem");
+    if (element.checked) {
+        arrow.style.transform = 'rotate(-180deg)';
+        secondaryHeaderItems?.forEach((header) => header.classList.remove("hidden"));
+    }
+    else {
+        arrow.style.transform = 'rotate(0deg)';
+        secondaryHeaderItems?.forEach((header) => header.classList.add("hidden"));
+    }
+};
+
+/** @title {{text}} {{underlineText}} */
+export interface CTA {
+    href: string;
+    text?: string;
+    underlineText?: string;
+    /** @format color-input */
+    backgroundColor?: string;
+    /** @format color-input */
+    textColor?: string;
+    /** @format color-input */
+    borderColor?: string;
+    ctaStyle: "button" | "link";
+    showIcon?: boolean;
+  }
+
+/** @title {{question}} */
+export interface Question {
+    question: string;
+    answear: HTMLWidget;
+}
+
+export interface Props {
+    title?: string;
+    /** @format color-input */
+    titleColor?: string;
+    caption?: string;
+    /** @format color-input */
+    captionColor?: string;
+    questions?: Question[];
+    /** @format color-input */
+    questionsTitleColor?: string;
+    /** @format color-input */
+    questionsDivisionLineColor?: string;
+    bottomTitle?: string;
+    /** @format color-input */
+    bottomTitleColor?: string;
+    bottomCaption?: string;
+    /** @format color-input */
+    bottomCaptionColor?: string;
+    cta?: CTA[];
+}
+
+export default function Faq({title, titleColor, caption, captionColor, questions = [], bottomTitle, bottomCaption, cta = [], questionsDivisionLineColor, questionsTitleColor, bottomCaptionColor, bottomTitleColor}: Props) {
+    return <div class="max-w-[1220px] mx-auto py-12 lg:py-20 px-7">
+        {title && <h2 class="text-2xl lg:text-5xl text-center leading-[120%] font-semibold mb-6" style={{color: titleColor}}>{title}</h2>}
+        {caption && <p class="text-base lg:text-lg text-center font-normal leading-none " style={{color: captionColor}}>{caption}</p>}
+        <div class="my-20 max-w-[768px] mx-auto">
+            {questions.map((collapse) => (<div className="collapse rounded-none border-b border-base-200" style={{borderColor: questionsDivisionLineColor}}>
+                <input type="checkbox" hx-on:change={useScript(onChange)}/>
+                <div className="collapse-title text-base lg:text-lg font-semibold flex px-7 py-6" style={{color: questionsTitleColor}}>
+                    {collapse.question}
+                    <div class="absolute top-8 right-4 transition-all duration-300 collapse-arrow ">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="fill-current" style={{stroke: questionsTitleColor}} xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.17674 11.5303L8.17676 11.5303L8.5303 11.1767C8.53031 11.1767 8.53031 11.1767 8.53032 11.1767C8.62794 11.0791 8.78621 11.0791 8.88385 11.1767C8.88385 11.1767 8.88385 11.1767 8.88385 11.1767L15.6464 17.9393L16 18.2929L16.3535 17.9393L23.1161 11.1767C23.2138 11.0791 23.372 11.0791 23.4696 11.1767L23.8232 11.5303C23.9208 11.628 23.9208 11.7862 23.8232 11.8838L16.1767 19.5303C16.0791 19.628 15.9209 19.628 15.8232 19.5303L8.17674 11.8838C8.17674 11.8838 8.17674 11.8838 8.17674 11.8838C8.07911 11.7862 8.07911 11.628 8.17674 11.5303Z" />
+                    </svg>
+                    </div>
+
+                </div>
+                <div className="collapse-content px-0" dangerouslySetInnerHTML={{__html: collapse.answear}} />
+            </div>))}
+        </div>
+        {bottomTitle && <p class="text-center text-2xl lg:text-[32px] leading-[130%] font-semibold" style={{color: bottomTitleColor}}>{bottomTitle}</p>}
+        {bottomCaption && <p class="text-center text-base lg:text-lg font-normal leading-normal mt-3" style={{color: bottomCaptionColor}}>{bottomCaption}</p>}
+        <AnimateOnShow divClass="flex flex-wrap items-center justify-center gap-7 mt-5" animation="animate-fade-up" delay={500}>
+            {cta.map((button) => {
+              if (button.href == '/talkToSpecialist') return <TalkToSpecialistCta
+                showIcon={button.showIcon}
+                underlineText={button.underlineText}
+                text={button.text}
+                ctaClass={`${button.ctaStyle != "link" && 'btn btn-primary px-7'} flex items-center gap-1 border-primary font-bold hover:scale-110 transition-transform text-base cursor-pointer`}
+                style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
+              />
+              return <a
+                href={button?.href ?? "#"}
+                target={button?.href.includes("http") ? "_blank" : ""}
+                class={`${button.ctaStyle != "link" && 'btn btn-primary px-7'} flex items-center gap-1 border-primary font-bold hover:scale-110 transition-transform text-base`}
+                style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
+              >
+                {button?.text}
+                {button.underlineText && <span class="underline">{button.underlineText}</span>}
+                {button.showIcon && <svg width="20" height="20" viewBox="0 0 20 20" class="fill-current" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15.3941 4.50977V12.2285C15.3941 12.386 15.3316 12.537 15.2202 12.6484C15.1089 12.7597 14.9579 12.8223 14.8004 12.8223C14.6429 12.8223 14.4919 12.7597 14.3805 12.6484C14.2692 12.537 14.2066 12.386 14.2066 12.2285V5.94293L5.72046 14.4298C5.60905 14.5413 5.45794 14.6038 5.30038 14.6038C5.14282 14.6038 4.99171 14.5413 4.8803 14.4298C4.76889 14.3184 4.7063 14.1673 4.7063 14.0098C4.7063 13.8522 4.76889 13.7011 4.8803 13.5897L13.3672 5.10352H7.08163C6.92416 5.10352 6.77313 5.04096 6.66178 4.92961C6.55043 4.81826 6.48788 4.66724 6.48788 4.50977C6.48788 4.35229 6.55043 4.20127 6.66178 4.08992C6.77313 3.97857 6.92416 3.91602 7.08163 3.91602H14.8004C14.9579 3.91602 15.1089 3.97857 15.2202 4.08992C15.3316 4.20127 15.3941 4.35229 15.3941 4.50977Z" />
+                </svg>}
+              </a>
+            })}
+          </AnimateOnShow>
+    </div>
+}

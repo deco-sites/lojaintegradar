e673ce14c186949a0ebd22591d0f53f0a480d507
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-11-10 14:37:52 -0300

    change: Header2 header message HtmlWidgets changed to RichText

--- sections/Header2.tsx
+++ sections/Header2.tsx
@@ -1,4 +1,4 @@
-import type { ImageWidget, HTMLWidget } from "apps/admin/widgets.ts";
+import type { ImageWidget, HTMLWidget, RichText } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
 import { useScript } from "@deco/deco/hooks";
@@ -34,7 +34,7 @@ const onLoad = (backgroundColor?: string, navigation?: Navigation) => {
 /** @title {{text}} {{underlineText}} */
 export interface CTA {
   href: string;
-  text?: HTMLWidget;
+  text?: RichText;
   underlineText?: string;
   /** @format color-input */
   backgroundColor?: string;
@@ -63,7 +63,8 @@ export interface CreateStoreWithPlanCTA {
 
 export interface HeaderMessage {
   show?: boolean;
-  text?: HTMLWidget;
+  text?: RichText;
+  font?: string;
   /** @format color-input */
   textColor?: string;
   /** @format color-input */
@@ -165,20 +166,20 @@ export default function Header2({ logo = {
     {campaignTimer?.show && <div class="h-[76px]" />}
     <div class="fixed top-0 left-0 w-full z-50 justify-center ">
       
-      {headerMessage?.show && <div class="min-h-16 w-full bg-primary text-primary-content px-11 py-2 flex items-center justify-center gap-1" style={{background: headerMessage?.backgroundColor, color: headerMessage?.textColor}}>
-        <p class="text-xs lg:text-base text-center font-semibold leading-[120%] flex items-center justify-center">
-          <div dangerouslySetInnerHTML={{__html: headerMessage.text || ""}}/>
-          {headerMessage.cta?.map((button) => (<a
+      {headerMessage?.show && <div class="min-h-16 w-full bg-primary text-primary-content px-11 py-2 flex items-center justify-center gap-1" style={{background: headerMessage?.backgroundColor, color: headerMessage?.textColor, fontFamily: headerMessage.font}}>
+        <p class="text-xs lg:text-2xl text-center font-semibold leading-[120%] flex items-center justify-center">
+          <div class="text-lg lg:text-2xl" dangerouslySetInnerHTML={{__html: headerMessage.text || ""}}/>
+          {headerMessage.cta?.map((button) => (<span><a
                 href={button?.href ?? "#"}
                 target={button?.href.includes("http") ? "_blank" : "_self"}
-                class={`ml-1 ${button.ctaStyle != "link" && 'btn btn-primary px-7 ml-4'} inline-flex  items-center gap-1 border-primary font-bold hover:scale-110 transition-transform text-xs lg:text-base`}
+                class={`ml-1 ${button.ctaStyle != "link" && 'btn btn-primary px-7 ml-4'} inline-flex  items-center gap-1 border-primary font-normal hover:scale-110 transition-transform text-xs lg:text-base`}
                 style={button.ctaStyle == "button" ? { backgroundColor: button.backgroundColor, color: button.textColor, borderColor: button.borderColor } : { color: button.textColor }}
               >
-                <div dangerouslySetInnerHTML={{__html: button.text || ""}}/>
+                <span class="text-lg lg:text-2xl" dangerouslySetInnerHTML={{__html: button.text || ""}}/>
                 {button.showIcon && <svg width="20" height="20" viewBox="0 0 20 20" class="fill-current" xmlns="http://www.w3.org/2000/svg">
                   <path d="M15.3941 4.50977V12.2285C15.3941 12.386 15.3316 12.537 15.2202 12.6484C15.1089 12.7597 14.9579 12.8223 14.8004 12.8223C14.6429 12.8223 14.4919 12.7597 14.3805 12.6484C14.2692 12.537 14.2066 12.386 14.2066 12.2285V5.94293L5.72046 14.4298C5.60905 14.5413 5.45794 14.6038 5.30038 14.6038C5.14282 14.6038 4.99171 14.5413 4.8803 14.4298C4.76889 14.3184 4.7063 14.1673 4.7063 14.0098C4.7063 13.8522 4.76889 13.7011 4.8803 13.5897L13.3672 5.10352H7.08163C6.92416 5.10352 6.77313 5.04096 6.66178 4.92961C6.55043 4.81826 6.48788 4.66724 6.48788 4.50977C6.48788 4.35229 6.55043 4.20127 6.66178 4.08992C6.77313 3.97857 6.92416 3.91602 7.08163 3.91602H14.8004C14.9579 3.91602 15.1089 3.97857 15.2202 4.08992C15.3316 4.20127 15.3941 4.35229 15.3941 4.50977Z" />
                 </svg>}
-              </a>))}
+              </a></span>))}
         </p>
       </div>}
 

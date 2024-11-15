07979079c4ce7b1c9eb205ab06f63cc2de75380f
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-11-15 20:23:29 -0300

    feat: Logos title font is now editable via admin props, Logos title is now a RichText, Logos bottom and padding are now editable via admin props

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -1,5 +1,5 @@
 import Image from "apps/website/components/Image.tsx";
-import type { ImageWidget } from "apps/admin/widgets.ts";
+import type { ImageWidget, RichText } from "apps/admin/widgets.ts";
 import AnimateOnShow from "../components/ui/AnimateOnShow.tsx"
 
 /** @title {{altText}} */
@@ -11,19 +11,22 @@ export interface Logo {
   height?: number;
 }
 export interface Props {
-  title?: string;
+  title?: RichText;
+  titleFont?: string;
   /** @format color-input */
   titleColor?: string;
-  caption?: string;
+  caption?: RichText;
   /** @format color-input */
   captionColor?: string;
   logos?: Logo[];
+  paddingTop?: string;
+  paddingBottom?: string;
 }
 const IMG_PLACEHODLER = Array(30).fill(0).map(() => ({
   src: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/03fbcc78-ca86-4616-a59a-b8aa18331a9c",
   altText: "Logo",
 }));
-export default function Logos({ title, caption, logos = IMG_PLACEHODLER, titleColor, captionColor }: Props) {
+export default function Logos({ title, caption, logos = IMG_PLACEHODLER, titleFont, titleColor, captionColor, paddingBottom, paddingTop }: Props) {
   logos = [...logos, ...logos];
   const slideContent = (<div id="logosSliderContent" class="flex items-center gap-7 md:gap-16">
     {logos?.map((logo) => {
@@ -32,14 +35,14 @@ export default function Logos({ title, caption, logos = IMG_PLACEHODLER, titleCo
       </AnimateOnShow>);
     })}
   </div>);
-  return (<div class="lg:container md:max-w-[1260px] lg:mx-auto px-7 md:px-0">
+  return (<div class="lg:container md:max-w-[1260px] lg:mx-auto px-7 md:px-0" style={{ paddingTop: paddingTop, paddingBottom: paddingBottom }}>
     <div class="flex flex-col gap-10">
       <div id="logosTitleAndCaption">
         <AnimateOnShow animation="animate-fade-up">
-          <p class="text-lg md:text-[32px] text-primary font-semibold " style={{ color: titleColor }}>{title}</p>
+          {title && <div class="text-lg md:text-[32px] text-primary font-normal " dangerouslySetInnerHTML={{ __html: title }} style={{ fontFamily: titleFont, color: titleColor }} />}
         </AnimateOnShow>
         <AnimateOnShow animation="animate-fade-up" delay={200}>
-          <p class="text-base md:text-2xl font-light mt-4" style={{ color: captionColor }}>{caption}</p>
+          <div class="text-base md:text-2xl font-light mt-4" style={{ color: captionColor }} dangerouslySetInnerHTML={{ __html: caption || "" }} />
         </AnimateOnShow>
       </div>
       <div class="relative w-full overflow-hidden h-20">

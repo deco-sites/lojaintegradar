60287266823e2f8655f1afd6ccc867ab7d6c5d24
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-11-15 21:11:29 -0300

    feat: Testimonials adjustments

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -1,4 +1,4 @@
-import type { ImageWidget, HTMLWidget } from "apps/admin/widgets.ts";
+import type { ImageWidget, HTMLWidget, RichText } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Slider from "../components/ui/Slider2.tsx";
 import { useId } from "../sdk/useId.ts";
@@ -53,8 +53,6 @@ export interface Content {
     nameBackgroundColor?: string;
     position?: string;
     /** @format color-input */
-    nameIconColor?: string;
-    /** @format color-input */
     quoteIconColor?: string;
 }
 /** @title {{content.alt}} */
@@ -62,9 +60,8 @@ export interface Testimonial {
     content?: Content;
 }
 export interface Props {
-    title?: string;
-    /** @format color-input */
-    titleColor?: string;
+    title?: RichText;
+    titleFont?:string;
     slides?: Testimonial[];
     /**
      * @title Show arrows
@@ -129,7 +126,7 @@ function SliderItem({ slide, id }: {
 }) {
     const { content, } = slide;
     return (<div id={id} class="relative w-full min-h-[292px]">
-    <div class="flex flex-col justify-end rounded-[32px] h-full shadow-spreaded2 overflow-hidden pt-7">
+    <div class="flex flex-col justify-between rounded-[32px] h-full shadow-spreaded2 overflow-hidden pt-7">
 
       <div class="px-6 md:pl-12 md:pr-7">
         <svg width="31" height="22" viewBox="0 0 31 22" class="text-primary fill-current" xmlns="http://www.w3.org/2000/svg" style={{ color: content?.quoteIconColor }}>
@@ -143,16 +140,7 @@ function SliderItem({ slide, id }: {
           <Image class="object-contain h-full" alt={content?.alt} src={content?.avatar || ""} width={content?.avatarWidth || 188} height={content?.avatarHeight || 68}/>
         </div>
         <div class="flex flex-col">
-          <div class="mt-5 flex items-center gap-1">
-            <svg height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" class="text-primary fill-current" viewBox="0 0 512 512" style={{ color: content?.nameIconColor }}>
-              <g>
-                <g>
-                  <path d="M256,0C114.62,0,0,114.62,0,256s114.62,256,256,256s256-114.62,256-256S397.38,0,256,0z M172.211,41.609 c-24.934,27.119-44.68,66.125-56.755,111.992H49.749C75.179,102.741,118.869,62.524,172.211,41.609z M25.6,256 c0-26.999,5.077-52.727,13.662-76.8h70.494c-4.608,24.294-7.356,49.963-7.356,76.8s2.748,52.506,7.347,76.8H39.262 C30.677,308.727,25.6,283,25.6,256z M49.749,358.4h65.707c12.083,45.867,31.821,84.872,56.755,111.991 C118.869,449.476,75.179,409.259,49.749,358.4z M243.2,485.188c-43.81-8.252-81.877-58.24-101.359-126.788H243.2V485.188z M243.2,332.8H135.74c-4.924-24.166-7.74-49.997-7.74-76.8s2.816-52.634,7.74-76.8H243.2V332.8z M243.2,153.6H141.841 C161.323,85.052,199.39,35.063,243.2,26.812V153.6z M462.251,153.6h-65.707c-12.083-45.867-31.821-84.873-56.755-111.992 C393.131,62.524,436.821,102.741,462.251,153.6z M268.8,26.812c43.81,8.252,81.877,58.24,101.359,126.788H268.8V26.812z M268.8,179.2h107.46c4.924,24.166,7.74,49.997,7.74,76.8s-2.816,52.634-7.74,76.8H268.8V179.2z M268.8,485.188V358.4h101.359 C350.677,426.948,312.61,476.937,268.8,485.188z M339.789,470.391c24.934-27.127,44.672-66.125,56.755-111.991h65.707 C436.821,409.259,393.131,449.476,339.789,470.391z M402.244,332.8c4.608-24.294,7.356-49.963,7.356-76.8 s-2.748-52.506-7.347-76.8h70.494c8.576,24.073,13.653,49.801,13.653,76.8c0,27-5.077,52.727-13.662,76.8H402.244z"/>
-                </g>
-              </g>
-            </svg>
-            <div class="font-semibold text-sm md:text-base" dangerouslySetInnerHTML={{ __html: content?.name || "" }}/>
-          </div>
+          <div class="font-semibold text-sm md:text-base" dangerouslySetInnerHTML={{ __html: content?.name || "" }}/>
           <p class="text-base">{content?.position}</p>
         </div>
       </div>
@@ -203,14 +191,13 @@ function Buttons({ arrowsColor }: {
 }
 function Carousel(props: Props) {
     const id = useId();
-    const { title, slides } = { ...DEFAULT_PROPS, ...props };
+    const { title, slides, titleFont } = { ...DEFAULT_PROPS, ...props };
     return (<AnimateOnShow animation="animate-fade-up50" delay={300}>
 
     <div id={id} class="min-h-min flex flex-col lg:container md:max-w-[1332px] lg:mx-auto pt-7 lg:pt-14" hx-on:click={useScript(refreshArrowsVisibility)} hx-on:touchend={useScript(refreshArrowsVisibility)}>
 
-      {title && <h2 class="text-4xl leading-snug lg:w-1/2 pb-12 lg:pb-16 text-primary" style={{ color: props.titleColor }}>
-        {title}
-      </h2>}
+      {title && <div class="text-4xl leading-snug lg:w-1/2 pb-12 lg:pb-16 text-primary" style={{ fontFamily:  titleFont}} dangerouslySetInnerHTML={{ __html: title}}/>}
+
       <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-9 pl-[30px] pr-[22px] py-9 md:px-9" rootId={id} interval={0 && 0 * 1e3} infinite>
         {slides?.map((slide, index) => (<Slider.Item index={index} class="carousel-item max-w-[608px] w-full">
           <SliderItem slide={slide} id={`${id}::${index}`}/>

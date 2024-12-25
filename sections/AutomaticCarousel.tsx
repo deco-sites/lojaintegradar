f7fbb45dc0e6b4057eed5878433be6d55c879c24
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-12-25 20:15:48 -0300

    style: AutomaticCarousel responsivity

--- sections/AutomaticCarousel.tsx
+++ sections/AutomaticCarousel.tsx
@@ -37,21 +37,9 @@ export interface IImage {
   height?: number;
 }
 
-export interface IVideo {
-  src?: VideoWidget;
-  width?: string;
-  height?: string;
-}
-
 export interface ITitle {
   text?: RichText;
   font?: string;
-}
-
-export interface IMedia {
-  image?: IImage;
-  video?: IVideo;
-  use?: "image" | "video";
 }
 
 export interface IBackgroundMedia {
@@ -63,7 +51,7 @@ export interface IBackgroundMedia {
 export interface Item {
   title?: ITitle;
   caption?: RichText;
-  media?: IMedia;
+  media?: IBackgroundMedia;
   /** @format color-input */
   textBackgroundColor?: string;
   /** @default true */
@@ -78,44 +66,46 @@ export interface Props {
 
 export default function AutomaticCarousel({ title, caption, items = [] }: Props) {
   const rootId = useId();
-  return <div id={rootId} class="relative min-h-[100vh]" style={{ height: `${items.length * 100}vh` }}>
+  return <div id={rootId} class={`relative min-h-[100vh] ${rootId}-container py-7 lg:py-0`}>
     <script
       type="module"
       dangerouslySetInnerHTML={{ __html: useScript(onLoad, rootId) }}
     />
-    <div class="sticky top-0 min-h-[100vh] flex items-end pb-7">
-      <div class="automaticCarousel overflow-auto flex items-center gap-24 w-full px-20" style={{ scrollbarWidth: "none" }}>
-        <div class="flex flex-col justify-center min-w-[500px] w-[486px]">
-          {title?.text && <div class="text-[70px] leading-[110%] font-normal" style={{ fontFamily: title.font }} dangerouslySetInnerHTML={{ __html: title.text }} />}
-          {caption && <div class="text-2xl leading-[120%] font-light mt-4" dangerouslySetInnerHTML={{ __html: caption }} />}
+    <style dangerouslySetInnerHTML={{
+      __html: `
+        @media (min-width: 1024px) {
+          .${rootId}-container {
+            height: ${items.length * 100}vh;
+          }
+        }
+      `}} />
+    <div class="sticky top-0 min-h-[100vh] flex items-end pb-7 w-full lg:w-auto">
+      <div class="automaticCarousel overflow-auto flex flex-col lg:flex-row items-center gap-4 lg:gap-24 w-full px-7 lg:px-20" style={{ scrollbarWidth: "none" }}>
+        <div class="flex flex-col justify-center lg:min-w-[500px] w-full lg:w-[486px]">
+          {title?.text && <div class="text-3xl lg:text-[70px] leading-[110%] font-normal" style={{ fontFamily: title.font }} dangerouslySetInnerHTML={{ __html: title.text }} />}
+          {caption && <div class="text-lg lg:text-2xl leading-[120%] font-light mt-4" dangerouslySetInnerHTML={{ __html: caption }} />}
         </div>
         {items.map((item) => (
           <div
-            class="relative flex items-end rounded-[45px] overflow-hidden"
-            style={{
-              minWidth: item.media?.use == "image" ? `${item.media?.image?.width || 942}px` : `${item.media?.video?.width || 942}px`,
-              width: item.media?.use == "image" ? `${item.media?.image?.width || 942}px` : `${item.media?.video?.width || 942}px`,
-              height: item.media?.use == "image" ? `${item.media?.image?.height || 729}px` : `${item.media?.video?.height || 729}px`
-            }}
+            class="relative flex items-end rounded-xl lg:rounded-[45px] overflow-hidden lg:min-w-[97vh] w-full lg:w-[97vh] h-[77vw] lg:h-[75vh]"
           >
             {item.media?.use == "image" && <Image
               src={item.media.image?.src || ""}
               alt={item.media.image?.alt || "Carousel image"}
               width={item.media?.image?.width || 942}
               height={item.media?.image?.height || 729}
-              class="absolute top-0 left-0 h-full w-full -z-50"
+              class="absolute top-0 left-0 h-full w-full -z-50 object-cover"
             />}
-            {item.media?.use == "video" && item.media.video?.src && <video width={item.media.video.width || 942} height={item.media.video.height || 729} autoPlay playsInline muted loading="lazy" loop
-              class="object-cover absolute top-0 left-0 h-full w-full -z-50"
-              style={{ width: item.media.video.width + "px" || "942px", height: item.media.video.height + "px" || "729px" }}>
-              <source src={item.media.video.src} type="video/mp4" />
+            {item.media?.use == "video" && item.media.video && <video width={942} height={729} autoPlay playsInline muted loading="lazy" loop
+              class="object-cover absolute top-0 left-0 h-full w-full -z-50">
+              <source src={item.media.video} type="video/mp4" />
             </video>}
             <div
-              class={`w-full ${item.textBackgroundColorDegrade ? 'pt-[157px]' : 'pt-11'} px-14 pb-11`}
+              class={`w-full p-4 ${item.textBackgroundColorDegrade ? 'pt-[100px] lg:pt-[157px]' : 'lg:pt-11'} lg:px-14 lg:pb-11`}
               style={{ background: item.textBackgroundColorDegrade ? `linear-gradient(to bottom, transparent 0%, ${item.textBackgroundColor} 50%)` : item.textBackgroundColor }}
             >
-              {item.title?.text && <div class="leading-[120%] text-4xl" dangerouslySetInnerHTML={{ __html: item.title.text }} style={{ fontFamily: item.title.font }} />}
-              {item.caption && <div class="text-xl leading-[120%] mt-5" dangerouslySetInnerHTML={{ __html: item.caption }} />}
+              {item.title?.text && <div class="leading-[120%] text-lg lg:text-4xl" dangerouslySetInnerHTML={{ __html: item.title.text }} style={{ fontFamily: item.title.font }} />}
+              {item.caption && <div class="text-sm lg:text-xl leading-[120%] mt-5" dangerouslySetInnerHTML={{ __html: item.caption }} />}
             </div>
           </div>
         ))}

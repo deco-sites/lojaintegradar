7b0bc9e6e8b4db421b8e7cae1fea371c706fef96
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-01-21 11:52:39 -0300

    fix: Header2 bars icon not working on the proxy

--- sections/Header2.tsx
+++ sections/Header2.tsx
@@ -319,7 +319,11 @@ export default function Header2({ logo = {
               })}
               </div>
               {!hideAsideMenu && <label htmlFor="mobile-drawer-nav" class={`btn btn-ghost drawer-button px-0`}>
-                <Icon id="Bars3" size={25} strokeWidth={0.1} class="text-primary fill-current" style={{color: barsColor}} />
+                {/* <Icon id="Bars3" size={25} strokeWidth={0.1} class="text-primary fill-current" style={{color: barsColor}} /> */}
+                <svg xmlns="http://www.w3.org/2000/svg" size={25} width={25} viewBox="0 0 20 20"  class="fill-current" style={{color: barsColor}}>
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 5.00033C2.5 4.54009 2.8731 4.16699 3.33333 4.16699H16.6667C17.1269 4.16699 17.5 4.54009 17.5 5.00033C17.5 5.46056 17.1269 5.83366 16.6667 5.83366H3.33333C2.8731 5.83366 2.5 5.46056 2.5 5.00033ZM2.5 10.0003C2.5 9.54009 2.8731 9.16699 3.33333 9.16699H16.6667C17.1269 9.16699 17.5 9.54009 17.5 10.0003C17.5 10.4606 17.1269 10.8337 16.6667 10.8337H3.33333C2.8731 10.8337 2.5 10.4606 2.5 10.0003ZM2.5 15.0003C2.5 14.5401 2.8731 14.167 3.33333 14.167H16.6667C17.1269 14.167 17.5 14.5401 17.5 15.0003C17.5 15.4606 17.1269 15.8337 16.6667 15.8337H3.33333C2.8731 15.8337 2.5 15.4606 2.5 15.0003Z" />
+                </svg>
+
               </label>}
             </ul>
             <ul class="hidden md:flex justify-end gap-7 flex-wrap">
@@ -357,7 +361,10 @@ export default function Header2({ logo = {
                 </a>
               })}
               {!hideAsideMenu && <label htmlFor="mobile-drawer-nav" class={`flex btn btn-ghost drawer-button px-0 order-last ${asideMenuOnlyMobile && 'lg:hidden'}`}>
-                <Icon id="Bars3" size={46} strokeWidth={0.1} class="text-primary fill-current" style={{ color: barsColor }} />
+                {/* <Icon id="Bars3" size={46} strokeWidth={0.1} class="text-primary fill-current" style={{ color: barsColor }} /> */}
+                <svg size={46} width={46} strokeWidth={0.1} class="text-primary fill-current" viewBox="0 0 20 20"  style={{ color: barsColor }}>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 5.00033C2.5 4.54009 2.8731 4.16699 3.33333 4.16699H16.6667C17.1269 4.16699 17.5 4.54009 17.5 5.00033C17.5 5.46056 17.1269 5.83366 16.6667 5.83366H3.33333C2.8731 5.83366 2.5 5.46056 2.5 5.00033ZM2.5 10.0003C2.5 9.54009 2.8731 9.16699 3.33333 9.16699H16.6667C17.1269 9.16699 17.5 9.54009 17.5 10.0003C17.5 10.4606 17.1269 10.8337 16.6667 10.8337H3.33333C2.8731 10.8337 2.5 10.4606 2.5 10.0003ZM2.5 15.0003C2.5 14.5401 2.8731 14.167 3.33333 14.167H16.6667C17.1269 14.167 17.5 14.5401 17.5 15.0003C17.5 15.4606 17.1269 15.8337 16.6667 15.8337H3.33333C2.8731 15.8337 2.5 15.4606 2.5 15.0003Z" />
+                </svg>
               </label>}
             </ul>
           </div>

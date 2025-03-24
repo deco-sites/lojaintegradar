44b959aa72b76cd6439c5cb08e49fdaf97f42c2b
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-03-23 21:37:08 -0300

    style: GridOverlay lines now has a shine where mouse is passing over

--- sections/GridOverlay.tsx
+++ sections/GridOverlay.tsx
@@ -1,4 +1,6 @@
 import { useScript } from "deco/hooks/useScript.ts";
+import Image from "apps/website/components/Image.tsx";
+import { isWindows } from "std/_util/os.ts";
 
 const onLoad = () => {
   // Pega a altura total do documento
@@ -8,21 +10,38 @@ const onLoad = () => {
   const GridOverlay = document.getElementById('GridOverlay') as HTMLElement;
   GridOverlay.style.height = documentHeight + 'px';
 
+  const followingMouseImage = document.getElementById('followingMouseImage') as HTMLElement;
+
+  document.addEventListener('mousemove', (evento) => {
+    const x = evento.clientX; // Coordenada X do mouse
+    const y = evento.clientY + globalThis.scrollY; // Coordenada Y do mouse
+
+    followingMouseImage.style.left = `${x - 100}px`;
+    followingMouseImage.style.top = `${y - 100}px`;
+  });
+
 };
 
-export default function GridOverlay() {
+export interface Props {
+  /** @format color-input */
+  linesColor?: string;
+  /** @format color-input */
+  mouseShineColor?: string;
+}
+
+export default function GridOverlay({ linesColor, mouseShineColor }: Props) {
   const SvgBackground = `<svg width="1440" height="5204" viewBox="0 0 1440 5204" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g opacity="0.6">
-      <path opacity="0.5" d="M0 855L1440 854.999" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M0 1942L1440 1942" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M0 3029L1440 3029" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M0 4116L1440 4116" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M0 5203L1440 5203" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M1 6432L1.00004 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M329 6432L329 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M657 6432L657 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M985 6432L985 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
-      <path opacity="0.5" d="M1313 6432L1313 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+    <g opacity="1">
+      <path opacity="1" d="M0 855L1440 854.999" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M0 1942L1440 1942" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M0 3029L1440 3029" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M0 4116L1440 4116" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M0 5203L1440 5203" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M1 6432L1.00004 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M329 6432L329 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M657 6432L657 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M985 6432L985 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
+      <path opacity="1" d="M1313 6432L1313 -1394" stroke="#00363A" stroke-width="0.780437" stroke-dasharray="4.68 4.68" />
     </g>
   </svg>`;
 
@@ -60,23 +79,28 @@ export default function GridOverlay() {
 </svg>
 `;
 
+
   return <div id="GridOverlay" class="absolute top-0 left-0 w-full h-full pointer-events-none flex justify-center overflow-hidden">
     <script
       type="module"
       dangerouslySetInnerHTML={{ __html: useScript(onLoad) }}
     />
+    <div class="h-full w-full flex justify-center relative" style={{ background: linesColor || 'rgba(0,0,0,0.3)' }}>
+      <div id="followingMouseImage" class="bg-red-600 h-[200px] w-[200px] absolute rounded-full blur-xl"
+        style={{ background: mouseShineColor || 'linear-gradient(white, rgb(0, 255, 193), rgb(0, 140, 232))' }} />
+    </div>
     <style
       dangerouslySetInnerHTML={{
         __html: `
         #GridOverlay {
-          background: url('data:image/svg+xml;utf8,${encodeURIComponent(SvgBackgroundMobile)}');
-          background-position: calc(50% + 0px) calc(0% - 0px);
+          mask: url('data:image/svg+xml;utf8,${encodeURIComponent(SvgBackgroundMobile)}');
+          mask-position: calc(50% + 0px) calc(0% - 0px);
         }
 
         @media (min-width: 1024px) {
           #GridOverlay {
-            background: url('data:image/svg+xml;utf8,${encodeURIComponent(SvgBackground)}');
-            background-position: calc(50% + 62px) calc(0% - 60px);
+            mask: url('data:image/svg+xml;utf8,${encodeURIComponent(SvgBackground)}');
+            mask-position: calc(50% + 62px) calc(0% - 60px);
           }
         }
         `}}

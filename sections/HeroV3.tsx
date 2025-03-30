701a63d1c1c5649c7cf0fc9a67e3552d9eed0661
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-03-29 21:31:25 -0300

    feat: CardsHero now have bottom cta

--- sections/HeroV3.tsx
+++ sections/HeroV3.tsx
@@ -113,7 +113,7 @@ export default function HeroV3({ title, text, bulletPoints, cta = [], media, con
   }
   return <div class="px-5 lg:px-0 relative">
     <div
-      class={`max-w-[1288px] relative z-10 mx-auto rounded-[20px] overflow-hidden p-[30px] lg:p-12 my-12 flex gap-5 flex-wrap-reverse lg:flex-nowrap items-center justify-center ${mediaPlacement[media?.placement || "right"]}`}
+      class={`max-w-[1288px] relative z-10 mx-auto rounded-[20px] overflow-hidden p-[30px] lg:p-12 my-12 flex gap-5 lg:gap-y-9 flex-wrap-reverse lg:flex-nowrap items-center justify-center ${mediaPlacement[media?.placement || "right"]}`}
       style={{ background: container?.backgroundColor, marginTop: container?.marginTop, marginBottom: container?.marginBottom, paddingTop: container?.paddingTop, paddingLeft: container?.paddingLeft, paddingBottom: container?.paddingBottom, paddingRight: container?.paddingRight, minHeight: container?.minHeight }}>
       <AnimateOnShow animation="animate-fade-up50" divClass={`${(media?.use && media.placement != "bellow") && 'max-w-[448px]'} w-full flex flex-col gap-6`} style={{ animationDuration: '1s', maxWidth: title?.titleMaxWidth }}>
 

12ac193e7477e1a982b294a37061e6a245101d13
decobot <capy@deco.cx>
2025-09-14 22:51:21 -0300

    feat: VerticalLines section maxHeight is now editable via admin props

--- sections/VerticalLines.tsx
+++ sections/VerticalLines.tsx
@@ -18,18 +18,19 @@ export interface Props {
   /** @description The number of time the image repeats */
   linesSize?: number;
   lines?: Line[];
+  maxHeight?: string;
 }
 
-export default function VerticalLines({lineImage, linesSize, lines}:Props) {
+export default function VerticalLines({lineImage, linesSize, lines, maxHeight}:Props) {
   return <div class="w-full relative">
-    <div class=" w-full absolute top-0 left-0 z-50 pointer-events-none">
+    <div class=" w-full absolute top-0 left-0 z-[1000] pointer-events-none">
       <div class="w-full relative">
         {lines?.map(line => {
           let styleClass;
           if (line.from == "left" || line.from == "right") styleClass = {[line.from || "left"]: line.distance};
           if (line.from == "center") styleClass = {left: `calc(50% + ${line.distance || "0px"} - ${lineImage.width ? `${lineImage.width / 2}px` : "0.5px"})`};
           return (
-            <div class={`absolute top-0`} style={styleClass}>
+            <div class={`absolute top-0 overflow-hidden `} style={{maxHeight, ...styleClass}}>
               {Array.from({ length: linesSize || 1 }).map((_, index) => (
                 <Image
                   key={index}

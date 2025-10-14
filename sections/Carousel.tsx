52f61e0f9db1da4d61c87c3992ee50a8dff4ed52
Henrry Bueno <henrrybueno.dev@gmail.com>
2025-10-14 19:31:00 -0300

    Ajustes fonts e imgs

--- sections/Carousel.tsx
+++ sections/Carousel.tsx
@@ -239,7 +239,7 @@ function Carousel(props: Props) {
     const carouselId = useId();
     return (<div id={id} style={{background: backgroundColor, paddingTop: paddingTop, paddingBottom: paddingBottom, marginTop}} class="relative pt-7 lg:pt-14">
         {/* <input type="text" value="0" /> */}
-            <div id={carouselId} class="min-h-min flex flex-col items-center w-full relative" hx-on:click={useScript(refreshArrowsVisibility)} hx-on:touchend={useScript(refreshArrowsVisibility)}>
+            <div id={carouselId} class="min-h-min flex flex-col items-center w-full relative" width="100%" height="100%" hx-on:click={useScript(refreshArrowsVisibility)} hx-on:touchend={useScript(refreshArrowsVisibility)}>
                 {backgroundImage?.src && <div class="absolute hidden md:block -z-50 top-0 left-0 h-full w-full"><Image src={backgroundImage.src} alt={backgroundImage.alt || "background image"} height={backgroundImage.height || 780} width={backgroundImage.width || 460} class="h-full object-contain" /></div>}
 
                 <AnimateOnShow animation="animate-fade-up" delay={200}>

21992b666cb9dcd904a63a688239b8d76b33f10c
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-10-23 18:40:49 -0300

    style: google login button width changed to 260

--- islands/CreateStoreModal.tsx
+++ islands/CreateStoreModal.tsx
@@ -239,7 +239,7 @@ const CreateStoreModal = ({googleAccountButton}: Props) => {
                     data-locale="pt-BR"
                     data-click_listener="signWithGoogle"
                     data-logo_alignment="center"
-                    data-width="364"
+                    data-width="260"
                 ></div>
             </div>
             <p class="w-full text-center text-sm">Ou crie com seu E-mail</p> </>}
